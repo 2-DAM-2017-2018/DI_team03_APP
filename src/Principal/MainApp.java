63ff17b7734e5d37e5fd6a3d8b4a1b42b5772953
Alejandro <alejandro.ariza@ponienteformacion.com>
2017-11-10 08:38:12 +0100

    Creacion de la estructura del proyecto

--- src/Principal/MainApp.java
+++ src/Principal/MainApp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package di_team03_app;
+package Principal;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/Vista/RootLayoutController.java
+++ src/Vista/RootLayoutController.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package di_team03_app;
+package Vista;
+
+import Principal.MainApp;
 
 /**
  * FXML Controller class
