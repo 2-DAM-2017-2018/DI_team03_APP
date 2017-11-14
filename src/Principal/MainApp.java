04c69235cc22f0397711c3d2fd7ee29d1cd0f5d9
Alejandro <alejandro.ariza@ponienteformacion.com>
2017-11-14 14:27:11 +0100

    Modificacion de MainApp y creación de AgregarSala

--- src/Principal/MainApp.java
+++ src/Principal/MainApp.java
@@ -5,10 +5,24 @@
  */
 package Principal;
 
+import Modelo.Recurso;
+import Vista.AgregarSalaController;
+import Vista.PieChartStatisticController;
+import Vista.PrincipalController;
+import Vista.RootLayoutController;
+import java.io.File;
+import java.io.IOException;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -16,24 +30,124 @@ import javafx.stage.Stage;
  * @author Alejandro
  */
 public class MainApp extends Application {
-    
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    private ObservableList<Recurso> datosRecursos = FXCollections.observableArrayList();
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("RootLayout.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Solicitud de recursos");
+
+        // set the application icon
+        this.primaryStage.getIcons().add(new Image("file:Recursos/MainIcon.png"));
+
+        initRootLayout();
+
+        showPersonOverview();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    public ObservableList<Recurso> getDatosRecursos() {
+        return datosRecursos;
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(personOverview);
+
+            PrincipalController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public boolean mostrarAgregarSala() {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/AgregarSala.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Agregar sala");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            AgregarSalaController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+
+            // Set the dialog icon.
+    //        dialogStage.getIcons().add(new Image("file:resources/images/edit.png"));
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
         launch(args);
-        
     }
-    //hola philip como estas
     
+    public void showPieChart() {
+       try {
+           FXMLLoader loader = new FXMLLoader();
+           loader.setLocation(MainApp.class.getResource("view/PieChartStatistic.fxml"));
+           AnchorPane page = (AnchorPane) loader.load();
+           Stage dialogStage = new Stage();
+           dialogStage.setTitle("Birthday Statistics");
+           dialogStage.initModality(Modality.WINDOW_MODAL);
+           dialogStage.initOwner(primaryStage);
+           Scene scene = new Scene(page);
+           dialogStage.setScene(scene);
+
+           PieChartStatisticController controller = loader.getController();
+           controller.setPersonData(personData);
+
+           dialogStage.show();
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+
 }
